053ee020eb09c081760d951a0b2de5baeea893eb
Renan Mitsuo <renan@softeasy.com.br>
2023-11-22 19:49:11 -0300

    Atualização do exercicio

--- cypress/integration/exercicio-e2e.spec.js
+++ cypress/integration/exercicio-e2e.spec.js
@@ -12,26 +12,18 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
 
     beforeEach(() => {
         cy.visit('/produtos')
-      
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
         let emailfaket = faker.internet.email()
         let nomefaker = faker.name.firstName()
         let sobrenomefaker = faker.name.lastName()
-        var quantidade = 7
-        cy.get('[class="product-block grid"]')
-        .contains('Bruno Compete Hoodie').click()
-        cy.get('.button-variable-item-S').click()
-        cy.get('.button-variable-item-Black').click()
-        cy.get('.input-text')
-        .clear()
-        .type(quantidade)
-        cy.get('.single_add_to_cart_button').click()
-        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+
+        cy.addProdutos('Bruno Compete Hoodie', 'M', 'Black', 3)
+        cy.visit('/produtos')
+        cy.addProdutos('Abominable Hoodie', 'M', 'Red', 3)
         cy.get('.woocommerce-message > .button').click()
         cy.get('.checkout-button').click()
-
         cy.get('#billing_first_name').type(nomefaker)
         cy.get('#billing_last_name').type(sobrenomefaker)
         cy.get('#select2-billing_country-container').click().type('Brasil', +'{enter}').click()
@@ -45,5 +37,5 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         cy.get('#terms').click()
         cy.get('#place_order').click()
         cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
-    });
+    });  
 })
